88f0edc2ca10aacb4c49a6d14c7ad26a711d1c2c
Jun Bon <junbon@binary.com>
2017-12-13 09:15:45 +0000

    fixes error in caching

--- lib/Finance/Calendar.pm
+++ lib/Finance/Calendar.pm
@@ -93,14 +93,14 @@ sub _get_cache {
 
     return unless exists $self->_cache->{$method_name};
 
-    my $key = join "_", ($exchange->symbol, (map { $_->truncate_to_day->epoch } @dates));
+    my $key = join "_", ($exchange->symbol, (map { $self->trading_date_for($exchange, $_)->epoch } @dates));
     return $self->_cache->{$method_name}{$key};
 }
 
 sub _set_cache {
     my ($self, $value, $method_name, $exchange, @dates) = @_;
 
-    my $key = join "_", ($exchange->symbol, (map { $_->truncate_to_day->epoch } @dates));
+    my $key = join "_", ($exchange->symbol, (map { $self->trading_date_for($exchange, $_)->epoch } @dates));
     $self->_cache->{$method_name}{$key} = $value;
 
     return;

92b38b4041c3e39c06d3636bf9c4ea94064ab17b
tanvir-deriv <tanvir@deriv.com>
2023-06-13 06:51:53 +0000

    [ci]Updated checking condition for exchanges with no tading breaks

--- lib/Finance/Calendar.pm
+++ lib/Finance/Calendar.pm
@@ -797,7 +797,7 @@ sub next_open_at {
             }
         }
 
-        unless (scalar $trading_breaks == 0) {
+        if (!@$trading_breaks) {
             my $opening_late = $self->opening_on($exchange, $date);
             return $opening_late;
         }
